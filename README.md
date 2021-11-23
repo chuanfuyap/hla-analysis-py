5e85570e79043df7116986cb29d065d66a4c6aea
chuanfuyap <yapchuanfu@gmail.com>
2021-11-23 14:18:47 +0000

    first commit for hapy

---
+++ README.md
@@ -0,0 +1,6 @@
+# haplotype-analysis-python (hapy)
+Various tools written in python to for haplotype analysis in genomics studies
+
+If there is interest in using the tools, feel free to clone the repository and install it by entering the corresponding directory containing the code via terminal and enter the following:
+
+`pip install .`
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_namespace_packages
+### Haplotype-Analysis 
+### install locally with 'pip install .'
+setup(name="hapy",
+        version="0.1dev1",
+        description='Tools to haplotype analysis for genomics studies.',
+        author='Chuan Fu Yap',
+        author_email='yapchuanfu@gmail.com',
+        license='MIT',
+        packages=find_namespace_packages(include=['hapy.data', "hapy.stats"]),
+        install_requires=['pandas', 'numpy', 'scipy', 'statsmodel'])
