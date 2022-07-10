3febdcee554b447d98a5afd4725c53fee54ff26d
chuanfuyap <yapchuanfu@gmail.com>
2022-07-10 21:19:40 +0100

    removed folder

--- build/lib/hapy/data/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-init for `data` subpackage for hapy package.
-"""
-__all__ = ["read_famfile", "read_bgl", "read_gprobs", "read_dosage", "HLAdat"]
-
-from hapy.data.io import *
-from hapy.data.HLAdat import HLAdata
--- build/lib/hapy/stats/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-init for `stats` subpackage for hapy package.
-"""
-__all__ = ["analyseAA", "analyseSNP", "analyseHLA", "survivalHLA",  "survivalAA", "processAnalysisInput_", "univariate_aminoacid"]
-
-from .stats import *
